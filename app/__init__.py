b4043112118ee7bee0349158110ec6a69d33c239
sysadmin <vladimirblanven@gmail.com>
2024-06-28 20:49:27 +0300

    Added error_handler

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
 from app.views import main_bp
 from app.extensions import db
+from pydantic import ValidationError
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
     db.init_app(app)
     app.register_blueprint(main_bp)
+
+    @app.errorhandler(ValidationError)
+    def handle_validation_error(error):
+        response = jsonify({"error": error.errors()})
+        response.status_code = 400
+        return response
+
+    @app.errorhandler(Exception)
+    def handle_exception(error):
+        response = jsonify({"error": "An unexpected error occurred", "message": str(error)})
+        response.status_code = 500
+        return response
+
     return app
