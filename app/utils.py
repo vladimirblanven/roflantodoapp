60544a6c2939b27a9891f2ab241ccc81e837b831
sysadmin <vladimirblanven@gmail.com>
2024-06-30 18:41:10 +0300

    Minor changes update

--- app/utils.py
+++ app/utils.py
@@ -4,12 +4,10 @@ from app.extensions import db
 def get_first_available_id():
     result = db.session.execute(text('SELECT id FROM todos ORDER BY id'))
     all_ids = [row[0] for row in result]
-    
+
     if not all_ids:
         return 1
-    
-    for i in range(1, len(all_ids) + 1):
+
+    for i in range(1, max(all_ids) + 2):
         if i not in all_ids:
             return i
-    
-    return all_ids[-1] + 1
--- runner.py
+++ runner.py
@@ -4,6 +4,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    host = os.getenv('FLASK_RUN_HOST')
-    port = int(os.getenv('FLASK_RUN_PORT'))
+    host = os.getenv('FLASK_RUN_HOST', '127.0.0.1')
+    port = int(os.getenv('FLASK_RUN_PORT', 5000))
     app.run(host=host, port=port)
